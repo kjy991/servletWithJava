7576f618e389cce098efdd6ea06fda50e5a04be4
엽킹 <kim91star2@naver.com>
2022-04-02 21:35:03 +0900

    servlet mvc패턴의 한계

--- src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -1,6 +1,5 @@
-package hello.servlet.domain.web;
+package hello.servlet.web.servlet;
 
-import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 
 import javax.servlet.ServletException;
